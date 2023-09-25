7bdae53097170698b4309ebf95c5ead8913ddd42
DevExpressExampleBot <fakeEmail>
2023-09-25 13:33:11 +0400

    Source auto update [skip ci]

--- VB/DXApplication3/Form1.Designer.vb
+++ VB/DXApplication3/Form1.Designer.vb
@@ -1,5 +1,7 @@
-﻿Namespace DXApplication3
-    Partial Public Class Form1
+Namespace DXApplication3
+
+    Partial Class Form1
+
         ''' <summary>
         ''' Required designer variable.
         ''' </summary>
@@ -10,14 +12,14 @@
         ''' </summary>
         ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Windows Form Designer generated code"
-
+'#Region "Windows Form Designer generated code"
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
@@ -25,8 +27,8 @@
         Private Sub InitializeComponent()
             Me.customGrid1 = New DXApplication3.CustomGrid()
             Me.customGridView1 = New DXApplication3.CustomGridView()
-            DirectCast(Me.customGrid1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(Me.customGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.customGrid1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.customGridView1), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
             ' 
             ' customGrid1
@@ -37,7 +39,7 @@
             Me.customGrid1.Name = "customGrid1"
             Me.customGrid1.Size = New System.Drawing.Size(632, 278)
             Me.customGrid1.TabIndex = 0
-            Me.customGrid1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.customGridView1})
+            Me.customGrid1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.customGridView1})
             ' 
             ' customGridView1
             ' 
@@ -52,16 +54,14 @@
             Me.Controls.Add(Me.customGrid1)
             Me.Name = "Form1"
             Me.Text = "Form1"
-            DirectCast(Me.customGrid1, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(Me.customGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.customGrid1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.customGridView1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
-
         End Sub
 
-        #End Region
+'#End Region
+        Private customGrid1 As DXApplication3.CustomGrid
 
-        Private customGrid1 As CustomGrid
-        Private customGridView1 As CustomGridView
+        Private customGridView1 As DXApplication3.CustomGridView
     End Class
 End Namespace
-
--- VB/DXApplication3/Form1.vb
+++ VB/DXApplication3/Form1.vb
@@ -1,8 +1,8 @@
-﻿Imports System
 Imports System.Data
 
 Namespace DXApplication3
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits DevExpress.XtraEditors.XtraForm
 
         Public Sub New()
@@ -10,15 +10,17 @@ Namespace DXApplication3
             customGrid1.DataSource = CreateTable(42)
             customGridView1.OptionsSelection.MultiSelect = True
         End Sub
+
         Private Function CreateTable(ByVal RowCount As Integer) As DataTable
-            Dim tbl As New DataTable()
+            Dim tbl As DataTable = New DataTable()
             tbl.Columns.Add("Name", GetType(String))
             tbl.Columns.Add("ID", GetType(Integer))
             tbl.Columns.Add("Number", GetType(Integer))
             tbl.Columns.Add("Date", GetType(Date))
             For i As Integer = 0 To RowCount - 1
-                tbl.Rows.Add(New Object() { String.Format("Name{0}", i), i, i Mod 4, Date.Now.AddDays(i) })
-            Next i
+                tbl.Rows.Add(New Object() {String.Format("Name{0}", i), i, i Mod 4, Date.Now.AddDays(i)})
+            Next
+
             Return tbl
         End Function
     End Class
